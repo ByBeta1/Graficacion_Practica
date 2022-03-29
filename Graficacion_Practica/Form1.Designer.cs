45bfdfe0b6ef3e7884175caa4d79166803ff1f33
ByBeta1 <100860273+ByBeta1@users.noreply.github.com>
2022-03-29 17:16:02 -0600

    Initial commit

---
+++ Graficacion_Practica/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Graficacion_Practica
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(12, 12);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(735, 319);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl1_OpenGLDraw);
+            this.openGLControl1.Resize += new System.EventHandler(this.openGLControl1_Resize);
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 343);
+            this.Controls.Add(this.openGLControl1);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "graficacion";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl1;
+    }
+}
+
---
+++ Graficacion_Practica/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace Graficacion_Practica
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openGLControl1_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        private void openGLControl1_Resize(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl1.OpenGL;
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            gl.Perspective(30.0f, (double)Width / (double)Height, 5, 100.0);
+
+            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+        }
+
+        private void openGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            SharpGL.OpenGL gl = this.openGLControl1.OpenGL;
+            gl.LoadIdentity();
+            gl.Translate(-2f, 0f, -6f);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            {
+                //Vértice 
+                gl.Vertex(-0.5f, 2.0f, 0.0f);
+                //Punto de extremo izquierdo      
+                gl.Vertex(0.5f, 2.0f, 0.0f);
+                //Punto final derecho       
+                gl.Vertex(0.5f, -2.0f, 0.0f);
+                gl.Vertex(-0.5f, -2.0f, 0.0f);
+                
+            }
+            gl.End();
+
+            gl.Translate(1.51f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            {
+                //Vértice 
+                gl.Vertex(-0.5f, 2.0f, 0.0f);
+                //Punto de extremo izquierdo      
+                gl.Vertex(0.5f, 2.0f, 0.0f);
+                //Punto final derecho       
+                gl.Vertex(0.5f, -2.0f, 0.0f);
+                gl.Vertex(-0.5f, -2.0f, 0.0f);
+
+            }
+            gl.End();
+            gl.Translate(1.52f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            {
+                //Vértice 
+                gl.Vertex(-0.5f, 2.0f, 0.0f);
+                //Punto de extremo izquierdo      
+                gl.Vertex(0.5f, 2.0f, 0.0f);
+                //Punto final derecho       
+                gl.Vertex(0.5f, -2.0f, 0.0f);
+                gl.Vertex(-0.5f, -2.0f, 0.0f);
+
+            }
+            gl.End();
+            gl.Translate(1.53f, 0f, 0f);
+            gl.Begin(OpenGL.GL_QUADS);
+
+            {
+                //Vértice 
+                gl.Vertex(-0.5f, 2.0f, 0.0f);
+                //Punto de extremo izquierdo      
+                gl.Vertex(0.5f, 2.0f, 0.0f);
+                //Punto final derecho       
+                gl.Vertex(0.5f, -2.0f, 0.0f);
+                gl.Vertex(-0.5f, -2.0f, 0.0f);
+
+               
+              
+            }
+            gl.End();
+            gl.Flush();
+        }
+    }
+    }
+
